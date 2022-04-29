38a7d8301416ce674a6b7af598900487fa44ccfd
HeeJooYoo <heejooyoo93@gmail.com>
2022-04-29 13:17:01 +0900

    Project Create

---
+++ src/main/java/com/shj/manonbakery/ManonbakeryApplication.java
@@ -0,0 +1,13 @@
+package com.shj.manonbakery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManonbakeryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManonbakeryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shj/manonbakery/ManonbakeryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shj.manonbakery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManonbakeryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
