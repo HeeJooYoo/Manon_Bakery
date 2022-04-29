7349434fdd9d7bd7407613b154f78b88ac4f6684
HeeJooYoo <heejooyoo93@gmail.com>
2022-04-29 15:20:04 +0900

    Encoding 및 DB 계정 설정

--- src/main/java/com/shj/manonbakery/ManonbakeryApplication.java
+++ src/main/java/com/shj/manonbakery/ManonbakeryApplication.java
@@ -2,8 +2,9 @@ package com.shj.manonbakery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class ManonbakeryApplication {
 
 	public static void main(String[] args) {
